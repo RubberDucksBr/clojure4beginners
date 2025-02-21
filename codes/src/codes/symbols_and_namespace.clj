662667d3220af08cdd3f1e1b6e6dd9f847d6e3ad
Daniele Araújo <s.araujodaniele@gmail.com>
2025-02-21 19:08:37 -0300

    Add code with condition and symbol examples

---
+++ codes/src/codes/symbols_and_namespace.clj
@@ -0,0 +1,15 @@
+(ns codes.symbols-and-namespace)
+
+; É raro precisar de um símbolo global para representar valores que não sejam funções.
+; Da mesma forma que em outras linguagens de programação, evitamos variáveis globais sempre que possível.
+; Quanto maior o escopo, menor o controle e maior a complexidade da manutenção do código.
+
+(defn valor-descontado
+  "Retorno do valor com o desconto de 10%"
+  [valor-bruto]
+  (let [taxa-de-desconto (/ 10 100)
+        desconto (* valor-bruto taxa-de-desconto)]          ; O let define um símbolo local que só é válido dentro do escopo do let
+    (- valor-bruto desconto)))                              ; O retorno é o resultado da última interação dentro do let
+
+(println (class 90N))
+(println (class 90M))
