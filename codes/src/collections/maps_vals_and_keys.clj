57ca2caf1e74d027b0a6989e721cfd45628673ea
Daniele Araújo <s.araujodaniele@gmail.com>
2025-02-21 21:36:25 -0300

    Add code with threading and comp examples

--- codes/src/collections/maps_vals_and_keys.clj
+++ codes/src/collections/maps_vals_and_keys.clj
@@ -20,8 +20,9 @@
 
 
 (def pedido {
-             :arroz  {:quantidade 2 :preco 10}
-             :feijao {:quantidade 3 :preco 20}})
+             :arroz    {:quantidade 2 :preco 10}
+             :feijao   {:quantidade 3 :preco 20}
+             :macarrao {:quantidade 1 :preco 0}})
 
 (println pedido)
 (println "Arroz" pedido :arroz)
@@ -41,3 +42,63 @@
     :quantidade
     inc
     println)
+
+
+; Desestruturação
+(defn imprime-e-15
+  [[chave valor]]
+  (println chave "<e>" valor)
+  15)
+
+(println (map imprime-e-15 pedido))
+
+(defn preco-dos-produtos
+  [[_ valor]]                                               ; Caso não queira usar a chave, pode-se usar o underline
+  (* (:quantidade valor) (:preco valor)))
+
+(println "Preço total do pedido" (reduce + (map preco-dos-produtos pedido)))
+
+
+
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map preco-dos-produtos pedido)))
+
+(println "Preço total do pedido" (total-do-pedido pedido))
+
+
+; Diferente do -> (threading first), o ->> (threading last) passa o resultado da expressão anterior como último argumento da próxima expressão.
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+       (map preco-dos-produtos,,,)
+       (reduce +,,,)))
+
+(println "Preço total do pedido" (total-do-pedido pedido))
+
+
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println (filter gratuito? (vals pedido)))
+
+
+
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println (filter (fn [[chave valor]] (gratuito? valor)) pedido))
+(println (filter #(gratuito? (second %)) pedido))
+
+
+(defn pago?
+  [item]
+  (not (gratuito? item)))
+
+(println (filter pago? (vals pedido)))
+
+; Trabalhando com composição de função
+(def pago? (comp not gratuito?))
+(println (filter pago? (vals pedido)))
